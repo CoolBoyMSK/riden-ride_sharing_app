aee8c1be7907ad7b0e1f1bd0e10cc7d29982057a
CoolBoyMSK <sohaibkhalid533@icloud.com>
2025-07-03 22:37:01 +0500

    file patch

--- src/dal/admin/adminOTP/index.js
+++ src/dal/admin/adminOTP/index.js
@@ -1,4 +1,4 @@
-import AdminOtpModel from '../../../models/AdminOtp.js';
+import AdminOtpModel from '../../../models/admin_otp.js';
 
 export const upsertResetOtp = (adminId, otp) =>
   AdminOtpModel.findOneAndUpdate(
