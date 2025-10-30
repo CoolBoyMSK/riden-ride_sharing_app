fa65841fc336be986ca712218fde4e4ff655ba68
CoolBoyMSK <sohaibkhalid533@icloud.com>
2025-10-30 19:49:06 +0500

    Fix: Server file cron fix

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import app from './src/app.js';
 import connectDB from './src/config/db.js';
 import startServer from './src/config/server.js';
-import crons from './src/crons/cronjobs.js';
+import './src/crons/cronjobs.js';
 
 const bootstrap = async () => {
   await connectDB();
   startServer(app);
-  crons;
 };
 
 bootstrap();
