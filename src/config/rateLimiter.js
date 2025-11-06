e029b9c243e49ef9daeae7d88f24ae9f03ac1c51
CoolBoyMSK <sohaibkhalid533@icloud.com>
2025-11-06 11:38:00 +0500

    Fix: rate Limiter Configuration

--- src/config/rateLimiter.js
+++ src/config/rateLimiter.js
@@ -4,7 +4,7 @@ const globalRateLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
   max: 100, // Limit each IP to 100 requests per window
   standardHeaders: true, // Add RateLimit-* headers
-  legacyHeaders: true, // Disable X-RateLimit-* headers
+  legacyHeaders: false, // Disable X-RateLimit-* headers
 });
 
 export default globalRateLimiter;
