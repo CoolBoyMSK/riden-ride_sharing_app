bb8ee6580d3ac92c4a0de9767baa7ff3d50d8a8a
CoolBoyMSK <sohaibkhalid533@icloud.com>
2025-11-17 19:32:32 +0500

    Logs: Driver locatio update logs

--- src/dal/ride.js
+++ src/dal/ride.js
@@ -437,6 +437,7 @@ export const getDriverLocation = async (driverId) => {
 
 export const persistDriverLocationToDB = async (driverId, { session } = {}) => {
   const data = await getDriverLocation(driverId);
+  console.log('Persisting Driver Location to DB:', data);
   if (!data) return null;
   return upsertDriverLocation(
     driverId,
