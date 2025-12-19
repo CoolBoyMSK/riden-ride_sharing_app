57769f399ab9d13e05a11dec4eaf39d44c740346
CoolBoyMSK <sohaibkhalid533@icloud.com>
2025-12-19 21:27:05 +0500

    fixed

--- src/enums/paymentEnums.js
+++ src/enums/paymentEnums.js
@@ -1,7 +1,7 @@
 export const CARD_TYPES = ['PERSONAL', 'BUSINESS'];
 export const CARD_BRANDS = ['Visa', 'MasterCard', 'Amex', 'Discover', 'Other'];
 export const WALLET_PROVIDERS = ['GOOGLE_PAY', 'APPLE_PAY'];
-export const PAYMENT_METHODS = ['CARD', 'GOOGLE_PAY', 'APPLE_PAY'];
+export const PAYMENT_METHODS = ['CARD', 'GOOGLE_PAY', 'APPLE_PAY', 'WALLET', 'CASH'];
 export const PAYMENT_STATUS = [
   'PENDING',
   'PROCESSING',
